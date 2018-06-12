4eb100d4880d70ba47d6b4410754add63f9f5099
likhithajames <34932184+likhithajames@users.noreply.github.com>
2018-06-12 15:25:58 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,10 +7,17 @@ using namespace std;
 // C++ program to find the execution time of code
 int main()
 {
+    int64_t prevTime;
     auto start = chrono::steady_clock::now();
-
+     int64_t now = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+     std::cout << now << std::endl;
     //sleep in seconds
     sleep(3);
+    if(prevTime!=now){
+    int64_t diff = now - prevTime;
+    prevTime = now;
+    std::cout << diff << std::endl;
+    }
     //sleep in micro seconds
     usleep(100000);
 
